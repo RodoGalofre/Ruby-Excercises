5202e8b7d9f53221ebd3ccf83b6dcd8471c67aca
rg <rodolfo.galofre@koombea.com>
2019-07-22 15:22:22 -0500

    modified Palindrome

--- LettersPalindrome.rb
+++ LettersPalindrome.rb
@@ -1,34 +1,7 @@
-# metodo que calcula si la word es un palindromo
-def calculate_palindrome(word)  
-    rever = word.reverse
-    puts (word == rever) ? "word #{word.upcase} is a palindrome" :  "#{word.upcase} is not a palindrome"
-    validate_keep_on()
+# metodo que calcula si la palabra es un palindromo
+def palindrome?(word)
+ word.reverse == word 
 end  
 
-@message = "do you want to know if your word is a palindrome?  "
+puts palindrome?('reconocer')
 
-# Metodo de entrada 
-def input_data(message)
-    puts "type word"
-    word = gets.chomp
-    @message = "Do you want to go on? "
-    calculate_palindrome(word) 
-end
-
-  
-# Metodo para confirmar si el usuario desea introducir más palabras
-def validate_keep_on()
-    puts @message
-    user_option = gets.chomp
-    if user_option == 'yes'
-  
-      input_data(@message)
-    else
-      puts "See you later "
-    end
-end
-  
-
-
-# llamado del metodo de entrada
-validate_keep_on()
