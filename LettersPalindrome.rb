2e03da84c72859dc33b628b39b9db4d2699e4bc3
rg <rodolfo.galofre@koombea.com>
2019-07-22 13:37:15 -0500

    Palindrome Exercise correted

--- LettersPalindrome.rb
+++ LettersPalindrome.rb
@@ -1,36 +1,34 @@
-
-@message = "¿quieres saber si tu palabra es un palindromo? "
-# Metodo de entrada 
-def input_data(message)
-    puts "Ingrese la palabra"
-    palabra = gets.chomp
-    @message = "¿quieres seguir?"
-    calcula_palin(palabra) 
-end
-
-# metodo que calcula si la palabra es un palindromo
-def calcula_palin(palabra)  
-  rever = palabra.reverse
-  puts (palabra == rever) ? "la palabra #{palabra.upcase} es un palindromo" :  "#{palabra.upcase} no es palindromo "
-  
-  validar_seguir()
+# metodo que calcula si la word es un palindromo
+def calculate_palindrome(word)  
+    rever = word.reverse
+    puts (word == rever) ? "word #{word.upcase} is a palindrome" :  "#{word.upcase} is not a palindrome"
+    validate_keep_on()
 end  
 
+@message = "do you want to know if your word is a palindrome?  "
 
-# Metodo para confirmar si el usuario desea introducir más palabras
-def validar_seguir()
-  
-  puts @message
-  user_option = gets.chomp
-  if user_option == 'si'
-
-    input_data(@message)
-  else
-    puts "See you later "
-  end
+# Metodo de entrada 
+def input_data(message)
+    puts "type word"
+    word = gets.chomp
+    @message = "Do you want to go on? "
+    calculate_palindrome(word) 
 end
 
+  
+# Metodo para confirmar si el usuario desea introducir más palabras
+def validate_keep_on()
+    puts @message
+    user_option = gets.chomp
+    if user_option == 'yes'
+  
+      input_data(@message)
+    else
+      puts "See you later "
+    end
+end
+  
 
 
 # llamado del metodo de entrada
-validar_seguir()
+validate_keep_on()
