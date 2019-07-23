e58455ace0f28593b31bb175c30e3e3be1e200c7
rg <rodolfo.galofre@koombea.com>
2019-07-23 11:36:36 -0500

    ready

--- Factorial_x_n.rb
+++ Factorial_x_n.rb
@@ -2,7 +2,7 @@ def factorial(n)
    factorial_capture = []
    factor = 1
    (1...n).each { |i| factorial_capture << factor *= i }
-   return factorial_capture
+   factorial_capture
 end
 
 puts factorial(7)
