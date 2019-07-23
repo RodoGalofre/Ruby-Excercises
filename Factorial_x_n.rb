e9b465ecf111c589bc4e2e77eddb5d13c40b3d9e
rg <rodolfo.galofre@koombea.com>
2019-07-23 16:09:35 -0500

    factorial exercise corrected

--- Factorial_x_n.rb
+++ Factorial_x_n.rb
@@ -1,8 +1,8 @@
 def factorial(n)
-   factorial_capture = []
-   factor = 1
-   (1...n).each { |i| factorial_capture << factor *= i }
-   factorial_capture
+  factorial_capture = []
+  factor = 1
+  (1...n).each { |i| factorial_capture << factor *= i }
+  factorial_capture
 end
 
 puts factorial(7)
