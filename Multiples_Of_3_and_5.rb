c320e2017bf21348848c4c31cf2b1916096f84c7
rg <rodolfo.galofre@koombea.com>
2019-07-24 13:22:09 -0500

    Sum Multiples of 3 and 5

--- Multiples_Of_3_and_5.rb
+++ Multiples_Of_3_and_5.rb
@@ -1,8 +1,14 @@
 
 def multiples_of_number(number1, number2)
     multiples_of_n = [] 
-    (1...1000).each {|i|  i % number1 == 0 ? multiples_of_n << i :  (i % number2 == 0 ? multiples_of_n << i : nil )}   
-     multiples_of_n.reduce :+
+    (1...1000).each { |i|  
+    if i % number1 == 0 
+      multiples_of_n << i 
+    elsif  i % number2 == 0 
+      multiples_of_n << i 
+    end
+    }   
+    multiples_of_n.reduce :+
 end
 
 puts multiples_of_number(3,5) 
