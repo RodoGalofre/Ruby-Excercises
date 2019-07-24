849e5206e4fd54365f479e3d5f5465be27251d06
rg <rodolfo.galofre@koombea.com>
2019-07-24 14:55:58 -0500

    Last prime number show

--- 10001stPrime.rb
+++ 10001stPrime.rb
@@ -1,17 +1,20 @@
-def see_prime(limit)
-  last_prime, cont, i = 0,0,1
+def show_prime(limit)
+  last_prime = 0
+  cont = 0
+  i = 1
   while cont <= limit
-    count, prime_validator = 2, true
-    while prime_validator && count < i
-     i % count == 0 ? prime_validator = false : count = count + 1  
-    end
-    if prime_validator 
-      (i > last_prime ? last_prime = i : nil )
-      cont += 1
-    end  
-  i += 1
+     count = 2
+     prime_validator = true
+      while prime_validator && count < i
+        i % count == 0 ? prime_validator = false : count = count + 1  
+        end
+        if prime_validator 
+        (i > last_prime ? last_prime = i : nil )
+        cont += 1
+      end  
+    i += 1
   end
-last_prime
+  last_prime
 end
 
-puts see_prime(10001)
+print "last prime number is ", show_prime(10001)
