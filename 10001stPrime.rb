19124cc18dca3dc2d58a75585a8afe4d0bc1ff70
rg <rodolfo.galofre@koombea.com>
2019-07-24 15:20:10 -0500

    redacted

--- 10001stPrime.rb
+++ 10001stPrime.rb
@@ -3,15 +3,15 @@ def show_prime(limit)
   cont = 0
   i = 1
   while cont <= limit
-     count = 2
-     prime_validator = true
-      while prime_validator && count < i
-        i % count == 0 ? prime_validator = false : count = count + 1  
-        end
-        if prime_validator 
-        (i > last_prime ? last_prime = i : nil )
-        cont += 1
-      end  
+    count = 2
+    prime_validator = true
+    while prime_validator && count < i
+      i % count == 0 ? prime_validator = false : count = count + 1  
+    end
+    if prime_validator 
+      (i > last_prime ? last_prime = i : nil )
+      cont += 1
+    end  
     i += 1
   end
   last_prime
