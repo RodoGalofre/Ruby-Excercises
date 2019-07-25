cefba688cd2e0c6db364e9879c82b21622f0f523
rg <rodolfo.galofre@koombea.com>
2019-07-25 09:29:19 -0500

    Fibonacci pars numbers remasterized

--- Fibonacci_numbers_par.rb
+++ Fibonacci_numbers_par.rb
@@ -1,12 +1,16 @@
-def fibonacci_pars
-  a = 0
-  b = 0
-  c = 1
-
-  (1...100).each do |i| 
-    a = b 
-    b = c 
-    c = a + b
-    return c if  c % 2 == 0
+def sum_fibonacci_even(target)
+  prev_num = 0
+  next_num = 0
+  fib_num = 1
+  fibo_pars = []
+  (1..100).each do |i| 
+    while fib_num <= target
+    prev_num, next_num = next_num, fib_num 
+    fib_num = prev_num + next_num
+    fib_num % 2 == 0 ?  fibo_pars << fib_num : nil
+    end
   end
+  fibo_pars.reduce :+
 end
+
+puts sum_fibonacci_even(4000000)
