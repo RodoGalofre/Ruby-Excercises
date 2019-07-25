513e082caff5efb2cf838eee0edd74d49cb8a7ac
rg <rodolfo.galofre@koombea.com>
2019-07-25 17:43:34 -0500

    remasterized  with poo

--- Fibonacci_numbers_par.rb
+++ Fibonacci_numbers_par.rb
@@ -1,17 +1,20 @@
 class FigonacciSums
-  def sum_fibonacci_even(target)
+  def initialize(target)
+    @target = target
+  end
+  def sum_fibonacci_even
     prev_num = 0
     next_num = 0
     fib_num = 1
     fibo_pars = []
-    while fib_num <= target
-      prev_num, next_num = next_num, fib_num 
+    while fib_num <= @target
+      prev_num, next_num = next_num, fib_num
       fib_num = prev_num + next_num
-      fib_num % 2 == 0 ?  fibo_pars << fib_num : nil
+      fibo_pars << fib_num  if fib_num % 2 == 0
     end
     fibo_pars.reduce :+
   end
 end
 
-sums = FigonacciSums.new
-puts sums.sum_fibonacci_even(4000000)
+sums = FigonacciSums.new(4000000)
+puts sums.sum_fibonacci_even
