57618e95b71e417ebf8225af12d31a25fd2a4f16
RodoGalofre <52467759+RodoGalofre@users.noreply.github.com>
2019-07-25 17:06:48 -0500

    update with poo

--- 10001stPrime.rb
+++ 10001stPrime.rb
@@ -1,20 +1,27 @@
-def show_prime(limit)
-  last_prime = 0
-  cont = 0
-  i = 1
-  while cont <= limit
-    count = 2
-    prime_validator = true
-    while prime_validator && count < i
-      i % count == 0 ? prime_validator = false : count = count + 1  
+class TenThousandPrimes
+  def show_prime(limit)
+    last_prime = 0
+    cont = 0
+    i = 1
+    while cont <= limit
+      count = 2
+      prime_validator = true
+      while prime_validator && count < i
+        if i % count == 0
+          prime_validator = false
+        else 
+          count = count + 1
+        end
+      end
+      if prime_validator
+        last_prime = i if i > last_prime
+        cont += 1
+      end
+      i += 1
     end
-    if prime_validator 
-      (i > last_prime ? last_prime = i : nil )
-      cont += 1
-    end  
-    i += 1
+    last_prime
   end
-  last_prime
 end
 
-print "last prime number is ", show_prime(10001)
+thouprint = TenThousandPrimes.new
+print "last prime number is ", thouprint.show_prime(10001)
