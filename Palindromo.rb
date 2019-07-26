e217fe779daccaddc8a3c2b0cb38b38ce3421bbb
rg <rodolfo.galofre@koombea.com>
2019-07-26 09:29:35 -0500

    Palindrome with POO

--- Palindromo.rb
+++ Palindromo.rb
@@ -1,16 +1,21 @@
-def three_digit_palindrome(limit)
-  last_palindrome = 0
-  for i in 100..limit
-    for j in 100..limit
-      possible_p = i*j
-      palindrome_valid = possible_p.to_s
-      if palindrome_valid == possible_p.to_s.reverse &&  palindrome_valid.to_i > last_palindrome.to_i
+class Palindrome
+  def initialize(limit)
+    @limit = limit
+  end
+  def last_palindrome
+    last_palindrome = 0
+    for i in 100..@limit
+      for j in 100..@limit
+        possible_p = i*j
+        palindrome_valid = possible_p.to_s
+        if palindrome_valid == possible_p.to_s.reverse &&  palindrome_valid.to_i > last_palindrome.to_i
           last_palindrome = palindrome_valid 
+        end
       end
     end
+    last_palindrome
   end
-  last_palindrome
 end
 
-print "palindromo mayor ",three_digit_palindrome(1000)
-
+three_digits = Palindrome.new(1000)
+print "palindromo mayor ", three_digits.last_palindrome
