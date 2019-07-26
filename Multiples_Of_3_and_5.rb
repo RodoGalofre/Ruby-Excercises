661ba032803e0c74e05e6919247bfdbe6aba1b8c
rg <rodolfo.galofre@koombea.com>
2019-07-26 08:57:18 -0500

    Multiples 3 and 5 corrected with POO

--- Multiples_Of_3_and_5.rb
+++ Multiples_Of_3_and_5.rb
@@ -4,15 +4,15 @@ class Mult
     @number2 = number2
   end
   def multiples_of_number
-      multiples_of_n = [] 
-      (1...1000).each do |i|  
-        if i % @number1 == 0 
-          multiples_of_n << i 
-        elsif  i % @number2 == 0 
-          multiples_of_n << i 
-        end
-      end   
-      multiples_of_n.reduce :+
+    multiples_of_n = [] 
+    (1...1000).each do |i|  
+      if i % @number1 == 0 
+        multiples_of_n << i 
+      elsif  i % @number2 == 0 
+        multiples_of_n << i 
+      end
+    end   
+    multiples_of_n.reduce :+
   end
 end
 
