9ab812dbce275b0d0e137f1802956a74f7e68dfa
rg <rodolfo.galofre@koombea.com>
2019-07-31 17:24:46 -0500

    test fibonacci

---
+++ Ruby-tests/fibonacci_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require './Fibonacci_numbers_par'
+require 'minitest/spec'
+
+describe FibonacciSums, 'Pars to limit fiibonacci' do
+  it 'is not a numbers' do
+    sums = FibonacciSums.new(4000000)
+    sums.sum_fibonacci_even.must_equal (4613732)
+  end
+  
+end
