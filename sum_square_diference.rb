f473fdf3cc8f1699bd86d92749eeff78658a7634
rg <rodolfo.galofre@koombea.com>
2019-07-31 16:59:29 -0500

    sum square modified

--- sum_square_diference.rb
+++ sum_square_diference.rb
@@ -1,22 +1,28 @@
-class SumSquares
-  def initialize(rang)
-    @rang = rang
-  end
-  def square_diferences
-    mayorsq = []
-    mayorsqs = []
-    square_of_the_sum = 0
-    sum_square = 0
-    diference = 0
-    (1..@rang).each do |i| 
-      sum_square += i**2
-      mayorsq << sum_square
-      xpotency = (square_of_the_sum += i)**2
-      mayorsqs << xpotency
+
+class TypeError < StandardError; end
+  class SumSquares
+    def initialize(rang)
+      @rang = parse_number(rang)
     end
-    mayorsqs.max - mayorsq.max 
+    def is_a_number?(rang )
+      rang.to_i > 0 
+    end
+    def parse_number(rang)
+      raise TypeError, 'That isn`t a number' unless is_a_number?(rang)
+      rang
+    end
+    def square_diferences
+      mayorsq = []
+      mayorsqs = []
+      square_of_the_sum = 0
+      sum_square = 0
+      diference = 0
+      (1..@rang).each do |i|
+        sum_square += i**2
+        mayorsq << sum_square
+        xpotency = (square_of_the_sum += i)**2
+        mayorsqs << xpotency
+      end
+      mayorsqs.max - mayorsq.max 
   end
 end
-
-squares = SumSquares.new(100)
-puts squares.square_diferences
