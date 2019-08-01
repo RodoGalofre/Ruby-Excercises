95800948e54c7f5dff8cd776d39c3316369b93ee
RodoGalofre <52467759+RodoGalofre@users.noreply.github.com>
2019-08-01 10:25:28 -0500

    fix redaction

--- Ruby-tests/sum_square_diference_test.rb
+++ Ruby-tests/sum_square_diference_test.rb
@@ -3,12 +3,16 @@ require './sum_square_diference'
 require 'minitest/spec'
 
 describe SumSquares, 'sum square diference exercise test' do
-  it 'is outcome-output' do
+  context 'when the class is initialize with 100' do
+  it 'is outcome 25164150' do
     squares = SumSquares.new(100)
     squares.square_diferences.must_equal (25164150)
   end
-  it "rang is not a number" do
+end
+context 'when the method is_a_number? not have a number' do
+  it "its return false" do
     squares = SumSquares.new(1)
     squares.is_a_number?('mmmm').must_equal (false)
   end
+ end
 end
