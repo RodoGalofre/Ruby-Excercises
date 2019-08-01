436fd3cbb9fc2e541dc5035201b9257f69c96c6e
RodoGalofre <52467759+RodoGalofre@users.noreply.github.com>
2019-08-01 09:56:25 -0500

    it was remaked

--- Ruby-tests/fibonacci_test.rb
+++ Ruby-tests/fibonacci_test.rb
@@ -3,9 +3,10 @@ require './Fibonacci_numbers_par'
 require 'minitest/spec'
 
 describe FibonacciSums, 'Pars to limit fiibonacci' do
-  it 'is a numbers' do
-    sums = FibonacciSums.new(4000000)
-    sums.sum_fibonacci_even.must_equal (4613732)
+  context 'when the class is initialize with 7' do
+    it 'sums_fibonacci_even is equal to 4613732' do
+      sums = FibonacciSums.new(4000000)
+      sums.sum_fibonacci_even.must_equal (4613732)
+    end
   end
-  
 end
