2a179af34b8ed3c9606632656f8c6d84013905f7
RodoGalofre <52467759+RodoGalofre@users.noreply.github.com>
2019-08-01 16:22:31 -0500

    new factorial test

--- Factorial_x_n.rb
+++ Factorial_x_n.rb
@@ -1,10 +1,16 @@
+class TypeError < StandardError; end
 class NumberFactor
-  def factorial(n)
-    (1..n).inject(1){ |fac,i| fac * i}
+  def initialize(n)
+    @n = error_not_number(n)
+  end
+  def valid_integer?(n)
+    n.is_a? Integer && n > 0 && n != n.nil?
+  end
+  def error_not_number(n)
+    raise TypeError, 'MUST BE A INTEGER NUMBER' unless valid_integer?(n)
+    n
+  end
+  def factorial
+    (1..@n).inject(1){ |fac,i| fac * i}
   end
 end
-
-factor = NumberFactor.new
-puts factor.factorial(7)
-
- 
