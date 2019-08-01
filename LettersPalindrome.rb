d71f9d905f983823cce0372feaded40854bbce35
rg <rodolfo.galofre@koombea.com>
2019-08-01 16:59:41 -0500

    Letters palindrome test

--- LettersPalindrome.rb
+++ LettersPalindrome.rb
@@ -4,20 +4,13 @@ class LettersPalindrome
     @word = parse_word(word)
   end
   def is_a_word?(word)
-    word.to_s != word.to_i.to_s 
+    word.to_s != word.to_i.to_s
   end
-
   def parse_word(word)
     raise TypeError, 'That is not a word' unless is_a_word?(word)
     word
   end
-
   def palindrome?
-    @word.reverse == @word 
+    @word.reverse == @word
   end
 end
-
-palind  = LettersPalindrome.new('word')
-#puts palind.palindrome?
-
-
--- Ruby-tests/LettersPalindrome-test.rb
+++ Ruby-tests/LettersPalindrome-test.rb
@@ -3,12 +3,10 @@ require './LettersPalindrome'
 require 'minitest/spec'
 
 describe LettersPalindrome, 'Palindrome tests' do
-
   it "is a palindrome " do
     palind = LettersPalindrome.new('reconocer')
     palind.palindrome?.must_equal (true)
   end
-    
   it "isn't a palindrome " do
     palind = LettersPalindrome.new('another')
     palind.palindrome?.must_equal (false)
@@ -17,5 +15,4 @@ describe LettersPalindrome, 'Palindrome tests' do
     err = lambda {palind = LettersPalindrome.new(432)}.must_raise (TypeError)
     err.message.must_match('That is not a word')
   end
-
 end
